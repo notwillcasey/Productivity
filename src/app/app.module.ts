c648e7842ade27ccbe32279fb4b2eed78c752302
notwillcasey <notwillcasey@gmail.com>
2023-12-24 19:59:04 -0700

    init containers for each component and set non standalone using app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
+// import { TaskFormComponent } from "./task-form/taskForm.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
